dacfac0068f7d5aaff072ae82a70c60bb4fcf6a1
Prashant Kumar <prashant.aman9@gmail.com>
2023-11-23 21:04:24 +0530

    Add react-pdf library

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,9 @@
 import { TextField, Button, Slider, Typography } from '@mui/material'
 import { useState } from 'react'
+import { PDFDownloadLink } from '@react-pdf/renderer'
+import LoadingButton from '@mui/lab/LoadingButton'
+import SaveIcon from '@mui/icons-material/Save'
+import PDFFile from './PDFFile'
 
 function Form() {
   const [formData, setFormData] = useState({
@@ -8,12 +12,14 @@ function Form() {
     mediumPercentage: 50,
     hardPercentage: 30,
   })
+  const [generateQP, setGenerateQP] = useState(false)
 
   const { totalMarks, easyPercentage, mediumPercentage, hardPercentage } =
     formData
 
   const handleChange = (e) => {
     const { name, value } = e.target
+    setGenerateQP(false)
     setFormData((prev) => ({
       ...prev,
       [name]: value,
@@ -23,6 +29,17 @@ function Form() {
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log({ formData })
+    setGenerateQP(true)
+  }
+
+  const handleReset = () => {
+    setFormData({
+      totalMarks: 100,
+      easyPercentage: 20,
+      mediumPercentage: 50,
+      hardPercentage: 30,
+    })
+    setGenerateQP(false)
   }
 
   return (
@@ -85,9 +102,43 @@ function Form() {
         max={100}
         sx={{ mb: 3 }}
       />
-      <Button variant="outlined" color="secondary" type="submit">
+      <Button variant="outlined" color="secondary" type="submit" sx={{ mr: 3 }}>
         Generate Question Paper
       </Button>
+
+      <Button
+        variant="outlined"
+        color="secondary"
+        type="button"
+        sx={{ mr: 3 }}
+        onClick={handleReset}
+      >
+        Reset
+      </Button>
+      {generateQP && (
+        <PDFDownloadLink document={<PDFFile />} fileName="questionpaper">
+          {({ loading }) =>
+            loading ? (
+              <LoadingButton
+                loading
+                loadingPosition="start"
+                startIcon={<SaveIcon />}
+                variant="outlined"
+              >
+                Loading...
+              </LoadingButton>
+            ) : (
+              <LoadingButton
+                loadingPosition="start"
+                startIcon={<SaveIcon />}
+                variant="outlined"
+              >
+                Save
+              </LoadingButton>
+            )
+          }
+        </PDFDownloadLink>
+      )}
     </form>
   )
 }
